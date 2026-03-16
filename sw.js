077b3b20c871bfc42e9c2ddb2dde42de5e2d6cd0
hautgamme <rynhr974@gmail.com>
2026-03-16 18:05:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'noisette-v2.0';
+const CACHE_NAME = 'noisette-v11';
 const assets = [
   './',
   './index.html',
@@ -7,26 +7,23 @@ const assets = [
   './logo.jpg',
   './fruit.webp',
   './croissant-.webp',
-  './varrines.webp'
+  './varrines.webp',
+  './patisserie.html',
+  './croissants.html',
+  './verrine.html'
 ];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
